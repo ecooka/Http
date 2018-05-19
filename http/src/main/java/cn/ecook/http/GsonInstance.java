bd0e1992aee7c4b4453800b1d968573aa08f60d3
ciba <ciba@ecook.cn>
2018-05-19 12:14:30 +0800

    first commit

---
+++ http/src/main/java/cn/ecook/http/GsonInstance.java
@@ -0,0 +1,21 @@
+package cn.ecook.http;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author ciba
+ * @date 2017/11/29
+ */
+
+public class GsonInstance {
+    private GsonInstance(){}
+
+    public static Gson instance(){
+        return InstanceHolder.instance;
+    }
+
+    static class InstanceHolder{
+        static final Gson instance = new Gson();
+    }
+}
